98f1a3c65cc204b1ffca65f4a330e261fd4eb782
qwdedefee <gary23381505@gmail.com>
2025-10-22 11:39:04 +0800

    week07

---
+++ week07/week07-1a.cpp
@@ -0,0 +1,15 @@
+///week07-1a.cpp TAICA NYCU Midterm Q.6
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<2*n;i++){
+        for(int j=1;j<2*n;j++){
+            if(j>1) cout<<" ";
+            cout<<n;
+        }
+        cout<<"現在是第i樓，i="<<i;
+        cout<<endl;
+    }
+}
